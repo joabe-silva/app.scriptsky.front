459d9bbdb576eba4a19126fec54bd33189d26623
Joabe Silva <joabesilva533@gmail.com>
2021-03-20 13:35:06 -0300

    adicionando botao para voltar para home na visualizacao detalhada do item

--- src/components/cliente/item/index.js
+++ src/components/cliente/item/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+import Fab from '@material-ui/core/Fab';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -11,6 +12,7 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Add from '@material-ui/icons/AddRounded';
 import Remove from '@material-ui/icons/RemoveRounded';
+import ArrowBack from '@material-ui/icons/ArrowBackIosRounded';
 import TextField from '@material-ui/core/TextField';
 import api from '../../../services/api';
 import './styles.css';
@@ -39,59 +41,66 @@ export default class Item extends Component {
     const { item } = this.state;
   
     return (
-    
-      <Card className="card">
-        <CardActionArea>
+      <div>
+        <Link to={'/'}>
+          <Fab size="small" color="primary" aria-label="add">
+            <ArrowBack />
+          </Fab>
+        </Link>
+        
+        <Card className="card">
+          <CardActionArea>
 
-          <CardMedia>
-            <img src={url_storage+item.imagem+url_complet} alt={ item.titulo } className="imagem" />
-          </CardMedia>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              { item.titulo }
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              { item.descricao }
-            </Typography>
-          </CardContent>
+            <CardMedia>
+              <img src={url_storage+item.imagem+url_complet} alt={ item.titulo } className="imagem" />
+            </CardMedia>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                { item.titulo }
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                { item.descricao }
+              </Typography>
+            </CardContent>
 
-        </CardActionArea>
-        <CardActions>
+          </CardActionArea>
+          <CardActions>
 
-          <Grid container spacing={2}>
-            <Grid item xs={12} className="actions">
-              <TextField
-                id="observacao"
-                label="Observação"
-                multiline
-                rowsMax={2}
-                className="observacao"
-              />  
+            <Grid container spacing={2}>
+              <Grid item xs={12} className="actions">
+                <TextField
+                  id="observacao"
+                  label="Observação"
+                  multiline
+                  rowsMax={2}
+                  className="observacao"
+                />  
+              </Grid>
+              <Grid item xs={12} className="actions">
+                <Button size="small" color="primary" >
+                  <Remove />
+                </Button>
+                <Button size="small" color="primary">
+                  { 1 }
+                </Button>
+                <Button size="small" color="primary">
+                  <Add />
+                </Button>
+              </Grid>
+              <Grid item xs={12} className="actions">
+                <Button size="small" color="primary">
+                  Adicionar
+                </Button>
+                <Button size="small" color="primary">
+                  R$ { item.preco }
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs={12} className="actions">
-              <Button size="small" color="primary" >
-                <Remove />
-              </Button>
-              <Button size="small" color="primary">
-                { 1 }
-              </Button>
-              <Button size="small" color="primary">
-                <Add />
-              </Button>
-            </Grid>
-            <Grid item xs={12} className="actions">
-              <Button size="small" color="primary">
-                Adicionar
-              </Button>
-              <Button size="small" color="primary">
-                R$ { item.preco }
-              </Button>
-            </Grid>
-          </Grid>
 
-        </CardActions>
-      </Card>
-    
+          </CardActions>
+        </Card>
+      </div>
+      
     )
 
   }
